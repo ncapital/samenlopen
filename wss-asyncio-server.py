bf291667bf37fc1cecf49f2188042c7f4f88f15b
ncapital <northern.capital.b.v@gmail.com>
2019-04-10 13:24:09 +0200

    added work from March: introducing websockets for faster collaborative threading

---
+++ wss-asyncio-server.py
@@ -0,0 +1,21 @@
+""" This file represents secured connection of Websockets. This should be more secured and perhaps faster than files or Redis.
+"""
+
+# WS server
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
