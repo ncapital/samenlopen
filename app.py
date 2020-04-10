daf72e54d39dad2910a82a87df0dbfe7411917c6
ncapital <northern.capital.b.v@gmail.com>
2020-04-10 17:28:10 +0200

    configuring server

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+
+# import sys
 from flask import Flask
 from views import controller_bp
+from common import keygens
 
-controller = Flask(__name__)
-controller.register_blueprint(controller_bp)
-controller.run(host='::', port=5000, use_reloader=True)
+app = Flask(__name__)
+# print(sys.argv)
+#
+# for arg in sys.argv[1:]:
+#     # load keys/secrets
+#     pass
+
+import IPython
+# IPython.embed(colors='neutral')
+IPython.embed()
+
+app.register_blueprint(controller_bp)
+app.run(host='::', port=5000, use_reloader=True, load_dotenv=True)
--- resources/__init__.py
+++ resources/__init__.py
@@ -5,14 +5,16 @@ import os
 from helpers import red
 
 keygens = {}
-resources_path = os.path.realpath(__name__)
-for resource in glob.glob(f"{resources_path}/[!_]*.py"):
-    # convert from resource.py to resource
-    resource = os.path.basename(resource)
-    resource = resource.replace('.py', '')
 
-    try:
-        module = importlib.import_module(f'resources.{resource}')
-        keygens[resource] = module.KeyGen()
-    except (ModuleNotFoundError, ImportError) as e:
-        print(red(f"WARNING! Can't import resource {resource} from resources!"))
+if __name__ == 'bla':
+    resources_path = os.path.realpath(__name__)
+    for resource in glob.glob(f"{resources_path}/[!_]*.py"):
+        # convert from resource.py to resource
+        resource = os.path.basename(resource)
+        resource = resource.replace('.py', '')
+
+        try:
+            module = importlib.import_module(f'resources.{resource}')
+            keygens[resource] = module.KeyGen()
+        except (ModuleNotFoundError, ImportError) as e:
+            print(red(f"WARNING! Can't import resource {resource} from resources!"))
